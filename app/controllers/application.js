4771f71498c8be7e4ba084a08019676325bcc55f
Navneet Singh <30341067+navneet-baidwan@users.noreply.github.com>
2017-07-28 23:04:58 -0700

    Update application.js

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,22 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  //initializing values
-  firstSentence: 'Welcome to',
-  lastSentence: 'TutorialsPoint!',
-
-  arrayOFgroup:['apple','pineapple','banana'],
-
-  actions: {
-    getLogin() {
-      $('#login.tiny.modal')
-  		.modal('show')
-		;
-    },
-     getRegister() {
-      $('#signup.tiny.modal')
-  		.modal('show')
-		;
-    }
-  }
-});
+export default Ember.Controller.extend({});
